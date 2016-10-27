349a1f467adbfe938ab8b194ccfd7b82c67e0d6f
James Barbour <jebarbou@cs.unc.edu>
2016-10-27 09:23:12 -0400

    hw12 completed-ish

---
+++ hw12/model-selection-and-anova.R
@@ -0,0 +1,13 @@
+gpa <- read.csv("College GPA.csv", header = TRUE)
+Y <- gpa$First.Yr.GPA
+X1 <- gpa$Math.SAT
+X2 <- gpa$Verbal.SAT
+X3 <- gpa$HS.Math.GPA
+X4 <- gpa$HS.English.GPA
+full = lm(Y ~ X1 + X2 + X3 + X4)
+summary(full)
+
+null_fit = lm(Y~1)
+anova(null_fit, full)
+
+fit_x1x2x3 = lm(Y~X1+X2+X3)
