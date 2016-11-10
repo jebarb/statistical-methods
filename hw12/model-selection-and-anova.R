3d1ca14129eb88b5a3274a331eeaa81be2dc7ac2
James Barbour <jebarbou@cs.unc.edu>
2016-11-10 10:19:39 -0500

    hw15 complete

--- hw12/model-selection-and-anova.R
+++ hw12/model-selection-and-anova.R
@@ -11,3 +11,19 @@ null_fit = lm(Y~1)
 anova(null_fit, full)
 
 fit_x1x2x3 = lm(Y~X1+X2+X3)
+anova(fit_x1x2x3, full)
+
+fit_x1x2x4 = lm(Y~X1+X2+X4)
+anova(fit_x1x2x4, full)
+
+fit_x1x3x4 = lm(Y~X1+X3+X4)
+anova(fit_x1x3x4, full)
+
+fit_x2x3x4 = lm(Y~X2+X3+X4)
+anova(fit_x2x3x4, full)
+
+fit_x1x2 = lm(Y~X1+X2)
+anova(fit_x1x2, full)
+
+fit_x3x4 = lm(Y~X3+X4)
+anova(fit_x3x4, full)
